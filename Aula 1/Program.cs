a824e73469e0ba231f8466470ec4d15b872a656a
FIC <FIC@info.local>
2019-09-13 11:14:31 -0300

    primeiro Programa funcionando

--- Aula 1/Program.cs
+++ Aula 1/Program.cs
@@ -22,11 +22,44 @@ namespace Aula_1
             //variável do tipo lógico(booleana)
             bool resultado = false;
 
+            //variavel do tipo texto (caracteres)
+            string nomeAluno;
 
-            Console.WriteLine(num2 + num5);
+            //quebra linha
+            Console.WriteLine();
+
+            Console.WriteLine("Bem vindo á nossa primeiro aplicação");
+
+            //continua escrevendo na mesma linha
+            Console.Write("Digite");
+            Console.Write("seu nome: ");
+
+            //capituro o dado digitado pelo usuário e salvo na variável
+            nomeAluno = Console.ReadLine();
+
+            //concatenação
+            Console.WriteLine(nomeAluno + ", agora digite sua primeira nota:");
+
+            // converto o texto capturado para o tipo inteiro
+            num3 = int.Parse(Console.ReadLine());
+
+            //interpolação
+            Console.WriteLine($"Sua primeira nota é {num3}");
 
 
-            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Digite sua segunda nota");
+            num4 = int.Parse(  Console.ReadLine()  );
+           
+            Console.WriteLine("Digite sua terceira nota: ");
+            num5 = int.Parse(   Console.ReadLine()  );
+
+
+            media = (num3 + num4 + num5) / 3;
+
+
+            Console.WriteLine("Sua media é:"  +  media);
+
+
         }
     }
 }
