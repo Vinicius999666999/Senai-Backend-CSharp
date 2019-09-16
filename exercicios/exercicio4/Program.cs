d663d30fbd701baffcf1d47ae150e8a7913c6a48
FIC <FIC@info.local>
2019-09-16 10:00:22 -0300

    exercicios

---
+++ exercicios/exercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double real, dolar;
+            double media;
+
+            Console.WriteLine("insiara valor ou real");
+            double = real = bouble.parse(Console.ReadLine());
+
+            Console.WriteLine("insira valor em dolar");
+            dolar = double.Parse(Console.ReadLine());
+
+            media = real / dolar ;
+
+            Console.WriteLine("valor da conversão é");
+
+
+
+
+
+        }
+    }
+}
