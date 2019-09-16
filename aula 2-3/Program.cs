06da2ed09dd1edaf7610b0f2c75b3ca1b06a8233
FIC <FIC@info.local>
2019-09-16 14:14:11 -0300

    estrutura simples

---
+++ aula 2-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace aula_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deixamos declarada como falsa por padão
+            bool entrada = false;
+            int idade;
+            string retorno = "entrada não permitida";
+
+            Console.WriteLine("digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade > 17 ){
+                entrada = true;
+            }
+
+            if (entrada == true){
+                retorno = "entrada permitida !!!";
+            }else{
+                retorno = "entrada não permitida :(";
+            }
+            
+            Console.WriteLine(retorno);
+
+        }
+    }
+}
