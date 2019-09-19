9da063d1debdf36def2f8e9595fa2ef6db3d964f
FIC <FIC@info.local>
2019-09-19 15:00:35 -0300

    atividade 2

---
+++ Aula 3 - Atividades/exercicio 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicio_2 {
+    class Program {
+        static void Main (string[] args) {
+            int user;
+            int tab;
+            int cont = 1;
+
+            Console.Write ("Digite um número: ");
+            user = int.Parse (Console.ReadLine ());
+
+            for (cont = 1; cont <= 10; cont++) {
+                tab = user * cont;
+
+                Console.WriteLine ($"{user} x {cont} = {tab}");
+            }
+        }
+    }
+}
