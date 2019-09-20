1a07ed407fc061736b0a1739411cd73a3cfce086
FIC <FIC@info.local>
2019-09-20 08:49:20 -0300

    atividade 3

---
+++ Aula 3 - Atividades/exercicio 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int user ;
+            int cont ;
+            int tab; 
+
+            Console.WriteLine("Digite o intervalo");
+            user = int.Parse (Console.ReadLine());
+
+            for(cont= 1 ; cont<= user ; cont++) {
+            tab = user * cont ;
+
+            Console.WriteLine ($"{5} x {cont} = {tab}");
+            
+            }
+
+        }
+    }
+}
