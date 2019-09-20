2a6d3916c1ea927c3222b25aa74040bb1b15cfef
FIC <FIC@info.local>
2019-09-20 14:04:49 -0300

    aula 4-2

---
+++ Aula 4-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula_4_2 {
+    class Program {
+        static void Main (string[] args) {
+            string[] series1 = new string[10];
+            string[] series2 = new string[10];
+
+            for (int i = 0; i <= 9; i++) {
+
+                Console.Write ("Digite um nome de uma serie: ");
+                series1[i] = Console.ReadLine ();
+            }
+
+            for (int i = 0; i <= 9; i++) {
+                series2[i] = series1[i];
+
+            }
+
+            for (int i = 0; i <= 9; i++) {
+                Console.WriteLine (series2[i]);
+            }
+
+        }
+    }
+}
