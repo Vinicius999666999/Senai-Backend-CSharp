6bd0b4800bca55c1c8a4bb70d7841b152c954bd3
FIC <FIC@info.local>
2019-09-20 10:16:33 -0300

    atividade 4

---
+++ Aula 3 - Atividades/exercicio 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercicio_4 {
+    class Program {
+        static void Main (string[] args) {
+            int user;
+            int cont;
+            double contacm = 0, acm = 0, media = 0;
+
+            for (cont = 1; cont <= 20; cont++) {
+                Console.Write ("digite um numero: ");
+                user = int.Parse (Console.ReadLine ());
+                if (user < 0) {
+                    contacm++;
+                    acm = acm = user;
+
+                } else {
+                    Console.WriteLine ("o número é ppsitivo"user);
+                }
+
+            }
+            media = acm / contacm;
+            Console.WriteLine (media);
+        }
+    }
+}
