9b433fd08a648e22eeab76bd5a74b94673643a9a
FIC <FIC@info.local>
2019-09-23 09:53:03 -0300

    aula 4-atividade 3

---
+++ Aula 4 - atividade/atividade 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace atividade_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numero = new double [10];
+
+            //para acum numeros do vetor
+            for(int i = 0 ; i <= 9; i++){
+                Console.WriteLine("Digite um valor");
+                numero[i] =double.Parse(Console.ReadLine());
+
+            }
+
+            for(int i = 0; i <= 9 ; i++){
+                Console.WriteLine(numero[i]);
+
+            }
+
+            Console.WriteLine(numero[9] * 5);
+            
+            
+
+        
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Aula 4 - atividade/atividade 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] produto = new string [12];
+            string [] cor = new string [12];
+            string [] tamanho = new string [12];
+            double [] preco = new double [12];
+
+            for(int i = 0 ; i <= 11 ; i++){
+                Console.WriteLine("Nome do produto: ");
+                produto[i] = (Console.ReadLine());
+
+                Console.WriteLine("Cor do produto: ");
+                cor[i] = (Console.ReadLine());
+
+                Console.WriteLine("Tamanho do produto: ");
+                tamanho[i] = (Console.ReadLine());
+
+                Console.WriteLine("Preco do Produto: ");
+                preco[i] = double.Parse(Console.ReadLine());
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
