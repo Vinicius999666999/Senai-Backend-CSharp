3dded5a6e3c0e8a2d85a5b8a7f94a30269675114
FIC <FIC@info.local>
2019-09-23 11:04:47 -0300

    aula 4 atividade 7

---
+++ Aula 4 - atividade/atividade 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace atividade_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vetor1 = new int[10];
+            int indice = 0;
+            int maior = 0;
+            int i = 0;
+
+            for(i = 0 ; i <= 9 ; i++){
+                Console.WriteLine("Insira um valor: ");
+                vetor1[i] = int.Parse(Console.ReadLine());
+
+                if (maior < vetor1[i]) {
+                    indice = i;
+                    maior = vetor1[i];
+                }
+            }
+
+            Console.WriteLine(maior + "é maio número , indice =" + indice);
+
+
+
+
+
+        }
+    }
+}
