0f5291b4ca6550b38db8c09ecf1de9a2175d7506
FIC <FIC@info.local>
2019-09-23 10:48:40 -0300

    aula 4 atividade 4

---
+++ Aula 4 - atividade/atividade 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace atividade_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] v1 = new string [15];
+            string[] v2 = new string [15];
+            string [] v3 = new string [30];
+
+            for (int i = 0 ; i <= 14 ; i++){
+                Console.WriteLine ("Digite palavras: ");
+                v1[i] = (Console.ReadLine());
+            }
+
+              for (int i = 0 ; i <= 14 ; i++){
+                Console.WriteLine ("Digite mas palavras: ");
+                v2[i] = (Console.ReadLine());
+            }
+
+            for ( int i = 15 ; i <= 29 ; i++) {
+                v3[i] = v1[i];
+            }
+
+            for(int i = 15 ; i <= 29 ; i++){
+                v3[i] = v2[i];
+            } 
+
+
+            for(int i = 0 ; i <= 29 ; i++){
+                Console.WriteLine (v3[i]);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
