bd53c7489f5fae9d3ed34586bc3c99409c540618
FIC <FIC@info.local>
2019-09-27 11:14:05 -0300

    Aula 9 atividade 2

---
+++ Aula 9 atividade/2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2 {
+    class Program {
+        static void Main (string[] args) {
+            ImprimiDataHora ();
+        }
+
+        static void ImprimiDataHora () {
+            DateTime hoje = DateTime.Today;
+
+            Console.WriteLine(hoje.DayOfWeek);
+        }
+
+    }
+}
